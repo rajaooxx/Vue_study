0a9573f75da613f25443cb42ecae374a57fca03b
Larry <fuchunmai@gmail.com>
2019-06-16 12:57:15 +0800

    init commit

---
+++ day3/component-ComboBox/component_combobox/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+ <div id="app">
+   <ComboBox></ComboBox>
+   <ComboBox></ComboBox>
+  </div>
+</template>
+
+<script>
+import ComboBox from './components/ComboBox'
+
+export default {
+  name: 'app',
+  components: {
+    ComboBox
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ day3/component-ComboBox/component_combobox/src/components/ComboBox.vue
@@ -0,0 +1,23 @@
+<template>
+   <div>
+      <span>Hello world</span>
+   </div>
+</template>
+
+
+<script>
+
+export default {
+  name: "ComboBox",
+  props : [],
+  methods : {
+
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
+
